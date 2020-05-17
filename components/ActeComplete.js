1f143e18bad804c3e578c252872f6c2b32a70e3d
agusticonesa <agusticonesagago@gmail.com>
2020-05-17 11:22:11 +0200

    ActeComplete arreglat

--- components/ActeComplete.js
+++ components/ActeComplete.js
@@ -82,37 +82,40 @@ export default class ActeComplete extends React.Component {
     if(this.state.acteLoaded){
       let Image_Http_URL = {uri: this.state.imatge};
       return (
-        <View style={styles.containerActeFlecha}>
+        <View style={styles.containerActeProva}>
+          <ScrollView style={styles.scrollView} onContentSizeChange={this.onContentSizeChange} showVerticalScrollIndicator={false}>
             <View style={styles.containerActe}>
               <Image source={Image_Http_URL} style={styles.image} rezideMode="center"></Image>
               <View style={styles.containerInfoActe}>
-                <View style={styles.WhereWhen}>
-                  <Text style={styles.Which}>
-                    {this.state.nomActivitat}
-                  </Text>
-                  <Text style={styles.Where}>
-                    {this.state.where}
-                  </Text>
-                  <Text style={styles.When}>
-                    {this.state.when}
-                  </Text>
-                </View>
-                <View style={styles.ActivitatCobles}>
+              <View style={styles.WhereWhen}>
+                <Text style={styles.Which}>
+                  {this.state.nomActivitat}
+                </Text>
+                <Text style={styles.Where}>
+                  {this.state.where}
+                </Text>
+                <Text style={styles.When}>
+                  {this.state.when}
+                </Text>
+              </View>
+              <View style={styles.ActivitatCobles}>
                   <Text style={styles.ActivitatCoblesInterpets}>Activitat:</Text>
                   <Text style={styles.InfoActeActivitatCobles}>{this.state.activitat}</Text>
-                </View>
-                <View style={styles.ActivitatCobles}>
+              </View>
+              <View style={styles.ActivitatCobles}>
                   <Text style={styles.ActivitatCoblesInterpets}>Cobles/Intèrprets:</Text>
                   <Text style={styles.InfoActeActivitatCobles}>{this.state.cobles}</Text>
-                </View>
-                <View style={styles.ActivitatCobles}>
-                  <Text style={styles.ActivitatCoblesInterpets}>Lloc si fa mal temps:</Text>
-                  <Text style={styles.InfoActeActivitatCobles}>{this.state.llocFaMalTemps}</Text>
-                </View>
-                <View style={{marginTop: 3}}></View>
-                  {this.mesInformacio()}
-                </View>
+              </View>
+              <View style={styles.ActivitatCobles}>
+                <Text style={styles.ActivitatCoblesInterpets}>Lloc si fa mal temps:</Text>
+                <Text style={styles.InfoActeActivitatCobles}>{this.state.llocFaMalTemps}</Text>
+              </View>
+              <View style={{marginTop: 3}}></View>
+               {this.mesInformacio()}
+               <View style={{marginBottom: 10}}></View>
+             </View>
             </View>
+          </ScrollView>
         </View>
       )
     }
@@ -135,22 +138,27 @@ ActeComplete.navigationOptions = {
 
 const styles = StyleSheet.create({
   containerActe: {
-    backgroundColor: 'grey',
-    marginTop:'14%',
     width:'100%',
     backgroundColor: "beige",
+    minHeight:552,
   },
   containerActeFlecha: {
     backgroundColor: 'grey',
     width:'100%',
     backgroundColor: "#714170",
   },
+  containerActeProva:{
+    flex: 1,
+    backgroundColor: '#714170',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop:'14%',
+  },
   image:{
     width:'100%',
-    height:'50%',
+    height:270,
   },
   containerInfoActe:{
-    height:'50%',
     width:'97%',
     justifyContent: 'center',
   },
@@ -201,10 +209,6 @@ const styles = StyleSheet.create({
     color:'grey',
     marginBottom:10,
     fontSize: 15,
-  },
-  scrollView:{
-    flex:1,
-    width:'100%',
   },
   scrollView:{
     flex:1,
